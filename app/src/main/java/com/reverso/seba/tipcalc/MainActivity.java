1c524011537c46850a1c9a749b3575d676ad268c
Sebastián Reverso <sheba1989@gmail.com>
2016-06-01 09:17:16 -0300

    Inyeccion de vistas y deteccion de click en submit

--- app/src/main/java/com/reverso/seba/tipcalc/MainActivity.java
+++ app/src/main/java/com/reverso/seba/tipcalc/MainActivity.java
@@ -1,19 +1,46 @@
 package com.reverso.seba.tipcalc;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.animation.Interpolator;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.inputBill)
+    EditText inputBill;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    @Bind(R.id.inputPercentage)
+    EditText inputPercentage;
+    @Bind(R.id.btnIncrease)
+    Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    Button btnDecrease;
+    @Bind(R.id.btnClear)
+    Button btnClear;
+    @Bind(R.id.txtTip)
+    TextView txtTip;
+
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_PRECENTAGE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -30,8 +57,25 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @OnClick(R.id.btnSubmit)
+    public void handleClickSubmit(){
+        Log.e(getLocalClassName(), "Click en Submit");
+        hideKeyboard();
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        try {
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (NullPointerException npe) {
+            Log.e(getLocalClassName(),Log.getStackTraceString(npe));
+        }
+    }
+
     private void about() {
-        TipCalcApp app = (TipCalcApp)getApplication();
+        TipCalcApp app = (TipCalcApp) getApplication();
         String strUrl = app.getAboutMe();
 
         Intent intent = new Intent(Intent.ACTION_VIEW);
